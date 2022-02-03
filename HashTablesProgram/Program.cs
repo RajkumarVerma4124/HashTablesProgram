fd7483aa9f978807887344b2ca08ccd61594b66a
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-03 13:10:59 +0530

    [Raj] Added:- The welcome message in the main program

---
+++ HashTablesProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The HashTable Program To Find Frequency Of Words");
+            Console.ReadLine();
+        }
+    }
+}
